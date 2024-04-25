c74c96dd21548a6019d8d09719121306b2bee995
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-25 14:53:38 +0200

    added route to about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TextParallaxContentExample from './Components/HomePage/HomePage'
+import AboutPage from './Components/AboutPage/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <TextParallaxContentExample />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TextParallaxContentExample />} />
+        <Route path='/about-us' element={<AboutPage/>} />
+        
+
+      </Routes>
+    </Router>
   );
 }
 
