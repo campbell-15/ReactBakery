b2cb22a942ca7ab4439fcc05406a5188b5b9fad4
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-26 20:51:42 +0200

    added nav to menu page

--- src/Components/MenuPage/MenuPage.jsx
+++ src/Components/MenuPage/MenuPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../Navbar'
 
 const MenuPage = () => {
     return (
         <div>
+            <Navbar/>
             whatsup
         </div>
     )
