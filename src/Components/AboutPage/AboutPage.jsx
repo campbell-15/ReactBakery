f2d267054432a5e50f35df367d037ea850c932c1
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-29 13:07:46 +0200

    imported css

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from '../Navbar'
+import React from 'react';
+import Navbar from '../Navbar';
+import './AboutPage.css';
 
 const AboutPage = () => {
   return (
